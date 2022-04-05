df9cebddafd04d2c0c64ffed894fc77ed2105c63
Fredy Gonzalez Garcia <fredylingling.fgg6@gmail.com>
2022-04-04 22:24:41 -0500

    first commit

---
+++ src/Header/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import "./header.css"
+
+
+
+
+export default function Header() {
+  return (
+    <div className="Navbar">
+
+    <div className="Navbar-Item">
+
+     <div className="Item1">Item1</div>
+     <div className="Item2">Item2</div>
+     <div className="Item3">Item3</div>
+     <div className="Item4">Item4</div>
+     
+    </div>  
+
+  </div>
+  )
+}
